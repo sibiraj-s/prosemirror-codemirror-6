5a6b16a706cfbf9c0040bbcbac6e252c1fcccde8
sibiraj-s <dev.sibiraj@outlook.com>
2021-01-26 11:26:19 +0530

    Update default doc

--- demo/doc.ts
+++ demo/doc.ts
@@ -7,13 +7,37 @@ export default {
   type: "doc",
   content: [
     {
-      type: "paragraph",
+      type: 'heading',
+      attrs: {
+        level: 2,
+        align: null
+      },
       content: [
         {
-          type: "text",
-          text: "This is editable text. You can focus it and start typing.",
+          type: 'text',
+          text: 'The code block is a code editor'
+        }
+      ]
+    },
+    {
+      type: 'paragraph',
+      attrs: {
+        align: null
+      },
+      content: [
+        {
+          type: 'text',
+          text: 'This editor has been wired up to render code blocks as instances of the CodeMirror'
         },
-      ],
+        {
+          type: 'text',
+          text: ' code editor, which provides syntax highlighting'
+        },
+        {
+          type: 'text',
+          text: ', auto-indentation, and similar.'
+        }
+      ]
     },
     {
       type: "code_mirror",

19b02f250db657ba82c73be1c4248191e21a5a36
Sibiraj <20282546+sibiraj-s@users.noreply.github.com>
2023-07-01 17:10:56 +0530

    Prefer vitest to jest

--- cm.spec.ts
+++ cm.spec.ts
@@ -1,4 +1,4 @@
-import { describe, it, expect } from '@jest/globals';
+import { describe, it, expect } from 'vitest';
 import { EditorState } from 'prosemirror-state';
 import { EditorView } from 'prosemirror-view';
 
